5460daf1807ae4dc6f8a3387361029307a780779
PEYRAT Aurélien <aure1814fr@gmail.com>
2024-05-14 05:01:25 +0200

    add script to insert project + update content

---
+++ script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// ********** CONSTANTS **********
+
+
+// ********** VARIABLES **********
+
+// ********** FUNCTIONS **********
+function displayProjects() {
+  const projectContainer = document.querySelector("#gallery");
+  projectContainer.innerHTML = "";
+
+  fetch('./project.json')
+  .then(response => response.json())
+  .then(data => {
+    
+    data.forEach(item => {
+      const projectCard = document.createElement("li");
+      projectCard.classList.add("project-card");
+      const projectTitle = document.createElement("h3");
+      const projectFigure = document.createElement("figure");
+      const projectCaption = document.createElement("figcaption");
+      const projectImage = document.createElement("img");
+      projectImage.classList.add("project-image");
+
+      projectTitle.textContent = item.title;
+      projectCaption.textContent = item.subtitle;
+      projectImage.src = item.cover;
+      projectImage.alt = item.alt;
+
+      
+      projectContainer.appendChild(projectCard);
+      projectCard.appendChild(projectTitle);
+      projectCard.appendChild(projectFigure);
+      projectFigure.appendChild(projectCaption);
+      projectFigure.appendChild(projectImage);
+    });
+  })
+  .catch(error => {
+    console.error('Erreur lors de la récupération du fichier JSON:', error);
+  });
+}
+
+function projectRedirect() {
+  //faire en sorte que si on clique sur l'image d'un projet, il redirige vers la page du projet
+}
+
+function addListeners() {
+  const projectCards = document.querySelectorAll(".project-image");
+}
+
+// ********** MAIN CODE **********
+
+displayProjects();
