a29706304d9bcb5424940594cdec586e407edc8c
PEYRAT Aurélien <aure1814fr@gmail.com>
2024-05-23 08:21:45 +0200

    add new skills + uptade project links

--- script.js
+++ script.js
@@ -18,12 +18,15 @@ function displayProjects() {
       const projectCard = document.createElement("li");
       projectCard.classList.add("project-card");
       const projectTitle = document.createElement("h3");
+      const projectLink = document.createElement("a");
       const projectFigure = document.createElement("figure");
       const projectCaption = document.createElement("figcaption");
       const projectImage = document.createElement("img");
       projectImage.classList.add("project-image");
 
       projectTitle.textContent = item.title;
+      projectLink.href = item.url;
+      projectLink.target = "_blank";
       projectCaption.textContent = item.subtitle;
       projectImage.src = item.cover;
       projectImage.alt = item.alt;
@@ -31,7 +34,8 @@ function displayProjects() {
       
       projectContainer.appendChild(projectCard);
       projectCard.appendChild(projectTitle);
-      projectCard.appendChild(projectFigure);
+      projectCard.appendChild(projectLink);
+      projectLink.appendChild(projectFigure);
       projectFigure.appendChild(projectCaption);
       projectFigure.appendChild(projectImage);
     });
